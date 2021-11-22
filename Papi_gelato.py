cae23da0549d7411fbee31bb6238711da3c9c1bc
Remastered Goldfish <99067800@mydavinci.nl>
2021-11-22 09:57:55 +0100

    ''

--- Papi_gelato.py
+++ Papi_gelato.py
@@ -2,23 +2,24 @@ def stap1():
     print('Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.')
     aantal = input('Hoeveel bolletjes wilt u? ')
     if aantal <= '3':
-        print('Dan krijgt u van mij ' + aantal + ' bolletjes')
+        print(f'Dan krijgt u van mij {aantal} bolletjes')
     elif aantal <= '8':
-        print('Dan krijgt u van mij ' + aantal + ' bolletjes')
+        print(f'Dan krijgt u van mij {aantal} bolletjes')
     elif aantal > '8':
         print('Sorry, zulke grote bakken hebben we niet')
-        stap1()
-    return
+        aantal = stap1()
+    return aantal
 
 def stap2():
+    aantal = stap1()
     houder = input("Wilt u deze aantal bolletje(s) in A: een hoorntje of B: een bakje? ")
     if houder == 'A':
-        print('Dan krijgt u van mij een hoortje')
+        print(f'Dan krijgt u van mij een hoortje met {aantal}')
     elif houder == 'B':
-        print('Dan krijgt u van mij een bakje')
+        print(f'Dan krijgt u van mij een bakje met {aantal}')
     elif print('Sorry, dat snap ik niet...'):
         stap2()
-    return
+    return 
 
 def stap3():
     bestellen = input('Hier is uw hoorntje/bakje met aantal bolletje(s).\n Wilt u nog meer bestellen?\n \n(Y/N)\n ')
@@ -30,6 +31,13 @@ def stap3():
         print('Sorry dat snap ik niet...')
         stap3()
 
-stap1()
-stap2()
-stap3()
+def extra():
+    topping = ['A', 'C', 'M', 'V']
+    for i in range(3):
+        smaken = input('Welke smaak wilt u voor bolletje nummber?\n A: Aardbei\n C: Chocolade \n M: Munt \n V: Vanille\n')
+    if smaken in topping:
+        print('')
+    else:
+        print('sorry ik snap het niet')
+    return
+
